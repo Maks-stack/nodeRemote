2897c86d1891ff821fca01105adc173e75ffb0cb
Maximilian Menschel <elmaxo@hotmail.de>
2016-11-27 17:59:12 +0100

    setup project

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var server =  http .createServer(app);
+var PORT = process.env.PORT || 2000;
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+app.use('/', express.static(__dirname));
+
+server.listen(PORT);
+console.log("server started on: " + PORT);
+
+var io = require('socket.io')(server,{});
+io.sockets.on('connection', function(socket){
+
+    console.log('socket connection');
+
+    socket.on('test', function(response){
+        console.log('test' + response.args);
+    });
+});
